3b3b3ff6fbc4c4efedca631b25f16f079f9b8b52
Greg Allen <greg@firstandthird.com>
2016-12-19 10:58:14 -0800

    fixes for prompt - allow blank and quote values

--- lib/prompt.sh
+++ lib/prompt.sh
@@ -2,6 +2,7 @@ prompt() {
   local question=$1
   local default=$2
   local var=$3
+  local allowBlank=$4
 
   if [[ -n "${!var}" ]]; then
     echo "$question: ${!var}"
@@ -9,13 +10,13 @@ prompt() {
     echo "$question [${var}=${default}]:"
     read answer
     if [[ -z "$answer" ]]; then
-      if [[ -z "$default" ]]; then
+      if [[ -z "$default" && "$allowBlank" != "true" ]]; then
         echo "must set value"
         exit 1
       fi
       eval "$var=$default"
     else
-      eval "$var=$answer"
+      eval "$var='$answer'"
     fi
   fi
 
