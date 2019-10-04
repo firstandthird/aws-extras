dbbb47fbca8ded08b014c019c06f41c0cd8be8be
Greg Allen <greg@firstandthird.com>
2019-10-04 12:10:37 -0700

    if redirecting output, just use defaults

--- lib/prompt.sh
+++ lib/prompt.sh
@@ -7,16 +7,20 @@ prompt() {
   if [[ -n "${!var}" ]]; then
     echo "$question: ${!var}"
   else
-    echo "$question [${var}=${default}]:"
-    read answer
-    if [[ -z "$answer" ]]; then
-      if [[ -z "$default" && "$allowBlank" != "true" ]]; then
-        echo "must set value"
-        exit 1
-      fi
+    if [[ -t 0 ]]; then
       eval "$var=$default"
     else
-      eval "$var='$answer'"
+      echo "$question [${var}=${default}]:"
+      read answer
+      if [[ -z "$answer" ]]; then
+        if [[ -z "$default" && "$allowBlank" != "true" ]]; then
+          echo "must set value"
+          exit 1
+        fi
+        eval "$var=$default"
+      else
+        eval "$var='$answer'"
+      fi
     fi
   fi
 
